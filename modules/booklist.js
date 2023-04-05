f4eb03d43b75728871de0cb1fe2f44f02ca3fd02
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-04-05 22:58:53 +0200

    Add files

---
+++ modules/booklist.js
@@ -0,0 +1,45 @@
+import Book from './book.js';
+
+export default class BookList {
+  constructor() {
+    this.storeData = JSON.parse(localStorage.getItem('Added Books')) || [];
+    this.container = document.querySelector('.book-list');
+    this.form = document.getElementById('form');
+    this.form.addEventListener('submit', this.addNewBook.bind(this));
+    this.container.addEventListener('click', this.removeBook.bind(this));
+
+    this.bookLink = document.getElementById('booksLink');
+    this.addBooksLink = document.getElementById('addNewBook');
+    this.contactLink = document.getElementById('contactLink');
+    this.booksSection = document.getElementById('books');
+    this.addBookSection = document.getElementById('addingBook');
+    this.contactSection = document.getElementById('contact');
+
+    this.bookLink.addEventListener('click', () => this.showBooksSection());
+    this.addBooksLink.addEventListener('click', () => this.showAddBookSection());
+    this.contactLink.addEventListener('click', () => this.showContactSection());
+
+    this.displayBooks();
+  }
+
+  addNewBook(event) {
+    event.preventDefault();
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const newBook = new Book(title, author);
+    this.storeData.push(newBook);
+    this.updateData();
+    this.displayBooks();
+    this.form.reset();
+  }
+
+  removeBook(event) {
+    if (event.target.classList.contains('remove')) {
+      const { index } = event.target.dataset;
+      this.storeData.splice(index, 1);
+      this.updateData();
+      this.displayBooks();
+    }
+  }
+
+  
